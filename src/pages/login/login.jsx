1270c3cd1fa93c5672c47e4c011518d25ff78ff5
Hrishivvarak <hrishikeshvarak20@gmail.com>
2024-08-25 14:16:18 +0530

    Hrishikesh

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -3,14 +3,6 @@
 const Login = () => {
   return (
     <div>login
-        ldkjsflksdjlvmsd
-        c
-        ccsdlvknsdlmc4sd;sd
-        sdmclkwdwmsv
-        dscvcnlksmd
-        dmcdsldmvca
-        sfcmsdlmc
-        cnsdlkn
     </div>
   )
 }
